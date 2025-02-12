9f30704d879cf3950a48448af41cdd93f4c4253a
Jesse <Existance@jesses-MacBook-Pro.local>
2025-02-12 11:33:26 -0500

    setting up apit fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react"
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from "./NavBar.jsx";
+
 
 const App = () => {
   const [ bookDetails, setBookDetails] = useState({});
 
-  
+
+  const getBooks = async() => {
+    const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
+    const jsonObj = await response.json();
+    
+  }
+getBooks();
   return (
     <>
-      <h1>Book Buddy</h1>
+      <NavBar />
+
+        <h1>Book Buddy</h1>
     </>
   )
 }
