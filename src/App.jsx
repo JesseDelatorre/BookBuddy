f0ae62529911bbb208352510eff39980b9abb221
Jesse <Existance@jesses-MacBook-Pro.local>
2025-02-12 10:51:59 -0500

    created navbar component and added useState to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
+import React from "react"
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  
+  const [ bookDetails, setBookDetails] = useState({});
 
+  
   return (
     <>
       <h1>Book Buddy</h1>
