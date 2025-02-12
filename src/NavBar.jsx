46187568f60774c61281279e4d13d8bf90ee5282
Jesse <Existance@jesses-MacBook-Pro.local>
2025-02-12 10:57:28 -0500

    added content to NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 
-  return(
-<>
-
-</>
+  return (
+    <nav>
+      <Link to='/'>Homepage</Link>
+      <Link to='/booklist'>Book List</Link>
+      <Link to='/Login'>Login</Link>
+    </nav>
 
   )
 }
+
+export default NavBar
